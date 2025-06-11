3c5a75f9500e6845c67ad54d5cdc654f739725c2
Yohanes-yhns <yohanes.mail1102@gmail.com>
2025-06-11 21:25:38 +0700

    membuat tampilan dengan ejs

--- app.js
+++ app.js
@@ -7,10 +7,14 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
+app.set('view engine','ejs');
+
 const routeCars = require('./routes/cars.route');
 const routeOrders = require('./routes/orders.route');
+const routeViews = require('./routes/views.routes');
 app.use(routeCars);
 app.use(routeOrders);
+app.use(routeViews);
 // const optionAllow = {
 //     origin: 'http://localhost:3000'
 // }
